5aae32ef6cc87af0fe709bece5a2c5eb4a0019f1
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-24 03:00:15 -0500

    Added comments

--- data/migrations/20200620021523_vegetables_table.js
+++ data/migrations/20200620021523_vegetables_table.js
@@ -1,8 +1,10 @@
 
+// up describes the changes that will be applied to the database
 exports.up = function(knex) {
   
 };
 
+// down describes how to undo the changes from the up function
 exports.down = function(knex) {
   
 };
