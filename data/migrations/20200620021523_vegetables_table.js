c30d5f812550105fd276d89606d0e54ef4fc7da4
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-24 03:12:25 -0500

    Added a drop table that deletes a table named vegetable

--- data/migrations/20200620021523_vegetables_table.js
+++ data/migrations/20200620021523_vegetables_table.js
@@ -2,14 +2,20 @@
 // up describes the changes that will be applied to the database
 exports.up = function(knex) {
   // we want to create a vegetables table
-  return knex.schema.createTable('vegetables', tbl => {
+  return knex.schema.createTable('vegetables', tbl => { // remember to return
     // needs a primary key, named id, type integer, autoincrement
     tbl.increments();// this method autoincrements the id number
-    tbl.string();
-  })
+    tbl
+    .string('name', 255)
+    .unique()
+    .index();
+    //up to 255 characters long, unique is a constraint so it cannot be repeated so database will give you an error if you type the same name when adding new values.
+    tbl.boolean('favorite'); 
+  });
 };
 
 // down describes how to undo the changes from the up function
 exports.down = function(knex) {
   // we want to delete(drop) the vegetables table
+  return knex.schema.dropTableIfExists('vegetables');
 };
