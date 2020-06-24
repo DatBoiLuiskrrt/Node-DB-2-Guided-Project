0cb42fcc4629b6c3024de1635e357b20fbd164af
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-24 03:06:58 -0500

    Created a table in my migrations

--- data/migrations/20200620021523_vegetables_table.js
+++ data/migrations/20200620021523_vegetables_table.js
@@ -1,10 +1,15 @@
 
 // up describes the changes that will be applied to the database
 exports.up = function(knex) {
-  
+  // we want to create a vegetables table
+  return knex.schema.createTable('vegetables', tbl => {
+    // needs a primary key, named id, type integer, autoincrement
+    tbl.increments();// this method autoincrements the id number
+    tbl.string();
+  })
 };
 
 // down describes how to undo the changes from the up function
 exports.down = function(knex) {
-  
+  // we want to delete(drop) the vegetables table
 };
