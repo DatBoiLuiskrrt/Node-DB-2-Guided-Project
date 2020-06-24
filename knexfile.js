668fffbebd9ccbd48b27ec765819dd4fa1bba180
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-24 03:32:56 -0500

    create a new table called market

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/produce.db3'
+      filename: './data/market.db3'
     },
     useNullAsDefault: true,
     migrations: {
